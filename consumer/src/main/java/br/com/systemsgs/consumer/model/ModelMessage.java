bcdf43aeb80eceb676b5bae14e403e04bde5b167
GuilhermeJWT <guiromanno@gmail.com>
2021-12-09 11:24:13 -0300

    Adicionando as Configurações no Consumer

---
+++ consumer/src/main/java/br/com/systemsgs/consumer/model/ModelMessage.java
@@ -0,0 +1,15 @@
+package br.com.systemsgs.consumer.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ModelMessage {
+
+    private int id;
+    private String name;
+
+}
